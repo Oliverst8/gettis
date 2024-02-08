1ccf066f1463aba63dd53d9280028aa229048218
iiinaD <dtha@itu.dk>
2024-02-08 16:12:58 +0100

    added sumbit()

--- src/sumbit.sh
+++ src/sumbit.sh
@@ -1,3 +1,12 @@
 
+submit(){
 
+        if [ -z "$1" ]; then
+        local java_file=$(getMainFile)
+    else
+        local java_file=$1
+    fi
+
+    python3 ~/bin/gettisSYS/submit.py $java_file
+}
 
